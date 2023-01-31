e0b3028541a2b092cb7466592d6474747f852d43
chunjaeilu <chunjaeilu@naver.com>
2023-01-31 15:23:15 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
   const [inputs, setInputs] = useState({
-    name: "이름",
-    nickname: "닉네임",
+    name: "",
+    nickname: "",
   });
   const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출(input.name 을 name으로 불러올 수 있다)
 
@@ -18,7 +18,20 @@ function App() {
       [name]: value, // name 키를 가진 값을 value로 설정
     });
   };
-  const onReset = () => {};
+
+  const nameInput = useRef(); // 선택한 DOM요소 ref객체 생성
+
+  // 리셋버튼 클릭시 실행되는 함수
+  const onReset = () => {
+    // 리셋버튼 클릭시 inputs 값 초기화
+    setInputs({
+      name: "",
+      nickname: "",
+    });
+
+    // ref로 선택한 DOM에 포커스 설정
+    nameInput.current.focus();
+  };
 
   return (
     <>
@@ -28,6 +41,7 @@ function App() {
         onChange={onChange}
         value={name}
         name="name" // name 속성을 부여해 키값을 참조할 수 있도록 한다
+        ref={nameInput} // 선택하고싶은 DOM에 ref값 설정
       />
       <input
         type="text"
