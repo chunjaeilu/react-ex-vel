19aa881b079821f747e88c3d190b750d37888c09
chunjaeilu <chunjaeilu@naver.com>
2023-01-31 23:45:13 +0900

    UserList2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 import UserList2 from "./components/UserList2";
 import CreateUser from "./components/CreateUser";
@@ -19,7 +19,9 @@ function App() {
       [name]: value,
     });
   };
-  const users = [
+  console.log("name:" + username, "email:" + email);
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
@@ -35,15 +37,29 @@ function App() {
       username: "liz",
       email: "liz@example.com",
     },
-  ];
+  ]);
 
   // Ref로 관리할 변수 생성
   const nextId = useRef(4);
-  onCreate = () => {
-    // 배열에 항목 추가하는 로직
+  console.log(nextId.current);
+  const onCreate = () => {
+    // 새로운 배열 항목
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+
+    // users 배열에 새로운 항목 추가
+    setUsers([...users, user]);
+
+    // input 초기화
     setInputs({ username: "", email: "" });
+
+    // 다음에 생성될 항목 id 변경
     nextId.current += 1;
   };
+
   return (
     <>
       <CreateUser
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,3 +1,19 @@
-export default function CreateUser() {
-  return <div>CreateUser</div>;
+export default function CreateUser({ username, email, onChange, onCreate }) {
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
 }
