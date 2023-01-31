a1382b96b67cbbd544425adefcf4c3e2a00f1815
chunjaeilu <chunjaeilu@naver.com>
2023-02-01 00:44:30 +0900

    배열 삭제하기

--- src/components/UserList2.js
+++ src/components/UserList2.js
@@ -1,24 +1,27 @@
 // import { useState, useRef } from "react";
 
-// User 컴포넌트 생성
-function User({ user }) {
-  return (
-    <div>
-      <b>{user.username}</b> <span>({user.email})</span>
-    </div>
-  );
-}
-
-export default function UserList2({ users }) {
+export default function UserList2({ users, onRemove }) {
   return (
     <>
       <h2>useRef로 컴포넌트 안의 변수 관리하기</h2>
       <div>
         {/* User 컴포넌트 불러오기, users 배열 참조하여 props 전달 */}
         {users.map((user) => (
-          <User key={user.id} user={user} />
+          <User key={user.id} user={user} onRemove={onRemove} />
         ))}
       </div>
     </>
+  );
+}
+
+// User 컴포넌트 생성
+// 자식의 자식 컴포넌트에 onRemove 전달
+function User({ user, onRemove }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+      {/* 컴포넌트에서 매개변수를 요청할 때는 콜백함수를 이용해야 한다 */}
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
   );
 }
