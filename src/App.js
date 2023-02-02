0b89f15c9657daac34fdbbfe205bbac2d9567df1
chunjaeilu <chunjaeilu@naver.com>
2023-02-02 12:44:55 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import React, { useState, useRef, useMemo, useCallback } from "react";
+import React, { useReducer, useRef, useMemo, useCallback } from "react";
 
 import UserList2 from "./components/UserList2";
 import CreateUser from "./components/CreateUser";
-import Counter from "./components/Counter";
 
 const countActiveUsers = (users) => {
   console.log("활성 사용자 수를 세는 중...");
@@ -11,23 +10,13 @@ const countActiveUsers = (users) => {
   // users 배열에서 active가 true인 새로운 배열을 추출하고, 배열의 길이를 리턴
 };
 
-function App() {
-  const [inputs, setInputs] = useState({
+// App에서 사용할 초기 상태를 컴포넌트 바깥으로 분리 (useReducer를 사용하는 목적이 컴포넌트와 상태 업데이트 로직을 분리하는 것이므로..)
+const initialState = {
+  inputs:{
     username: "",
     email: "",
-  });
-
-  const { username, email } = inputs;
-
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    setInputs((inputs) => ({
-      ...inputs,
-      [name]: value,
-    }));
-  }, []);
-
-  const [users, setUsers] = useState([
+  },
+  users:[
     {
       id: 1,
       username: "velopert",
@@ -45,60 +34,96 @@ function App() {
       username: "liz",
       email: "liz@example.com",
       active: false,
-    },
-  ]);
+    }
+  ]
+}
 
-  // Ref로 관리할 변수 생성
+// reducer() 함수 생성
+function reducer(state, action) {
+  switch (action.type) {
+    // action 객체의 type이 'CHANGE_INPUT'일때 실행 (onChange)
+    case 'CHANGE_INPUT':
+      return {
+        ...state, // state 객체를 spread 연산자로 펼침
+        inputs: { // state.inputs 선택
+          ...state.inputs, // state.inputs를 spread 연산자로 펼침
+          [action.name]: action.value // action객체의 name을 선택(이름을 입력하면 name: username, 메일주소를 입력할 때는 name: email)해서 action 객체의 value 값을 반영
+        }
+      };
+    // action 객체의 type이 'CREATE_USER'일때 실행 (onCreate)
+    case 'CREATE_USER':
+      return {
+        // inputs 초기화
+        inputs : initialState.inputs,
+        // users 업데이트
+        users: state.users.concat(action.user)
+      };
+    // action 객체의 type이 'REMOVE_USER'일때 실행 (onRemove)
+    case 'REMOVE_USER':
+      return {
+        ...state, // state 객체를 spread 연산자로 펼침
+        users: state.users.filter(user => user.id !== action.id) // users 선택, filter함수를 이용해 아이디값이 전달받은 action.id값(선택한 id값)과 일치하지 않는 users 배열을 리턴
+      };
+    // action 객체의 type이 'TOGGLE_USER'일때 실행 (onToggle)
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => user.id === action.id ? {...user, active: !user.active } : user)
+      }
+    default:
+      return state;  
+  }
+}
+
+function App() {
+  // initialState 객체를 초기값으로 가지는 state 를 useReducer로 관리하겠다는 선언
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  // onChange 함수
+  const onChange = useCallback(e => {
+    const {name, value} = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  },[])
+
+  // onCreate 함수
   const onCreate = useCallback(() => {
-    // 새로운 배열 항목
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-
-    // users 배열에 새로운 항목 추가
-    // setUsers([...users, user]);
-    setUsers((users) => users.concat(user));
-
-    // input 초기화
-    setInputs({ username: "", email: "" });
-
-    // 다음에 생성될 항목 id 변경
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    });
     nextId.current += 1;
-  }, [username, email]);
+  },[username, email])
 
+  // onRemove 함수
   const onRemove = useCallback((id) => {
-    // filter함수를 이용, user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // 결과적으로 user.id 가 id 인 것을 제거함
-    // setUsers(users.filter((user) => user.id !== id));
-    setUsers((users) => users.filter((user) => user.id !== id));
-  }, []);
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  },[])
 
+  // onToggle 함수
   const onToggle = useCallback((id) => {
-    // setUsers(
-    // map함수는 특정인자를 바꾸고 새로운 배열을 출력한다
-    // setUsers 내부에 map함수를 사용하면 결과적으로 바뀐 새로운 배열이 users 변수에 대입된다
-    // users.map(
-    // (user) => (user.id === id ? { ...user, active: !user.active } : user)
-    // user.id === id ? | users 배열 중 id가 매개변수로 전달받은 id와 일치하면
-    // { ...user, active: !user.active } | 해당 user의 active 속성을 현재와 반대로 전환(true >> false, false >> true)시키고
-    // : user | 일치하지 않는 user는 그대로 출력함
-    // )
-    // );
-    setUsers((users) =>
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  }, []);
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    })
+  },[])
 
-  // const count = countActiveUsers(users);
-  // useMemo(()=>{실행코드;},[deps])
+  // countActiveUsers 함수 실행
   const count = useMemo(() => countActiveUsers(users), [users]);
-  // users의 내용이 바뀔 때만 countActiveUsers() 함수를 실행
 
   return (
     <>
@@ -108,13 +133,13 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList2 users={users} onRemove={onRemove} onToggle={onToggle} />
+      <UserList2
+        users={users}
+        onRemove={onRemove}
+        onToggle={onToggle}
+      />
 
       <div>활성 사용자 수 : {count}</div>
-
-      <hr />
-
-      <Counter />
     </>
   );
 }
